a1d1a23a13c398d8e962c2d17012d73c0cd3fd8f
tejas1coditas <tejas.patange@coditas.com>
2025-07-14 10:02:29 +0530

    changes

--- number_analyzer.dart
+++ number_analyzer.dart
@@ -3,12 +3,17 @@ import 'dart:math';
 
 void main() {
   try {
-    print("Type a whole number");
-    int? n = int.parse(stdin.readLineSync()!);
+    print('Enter Your Number : ');
+    String? num = stdin.readLineSync();
+    if (num == null || num.trim().isEmpty) {
+      print('Invalid Input !');
+    }
+    int n = int.parse(num!.trim());
     if (n < 0) throw ArgumentError("-ve numbers are not allowed");
     print("your input is $n");
 
     print(chkNonNull(n));
+    print(ans(n));
     print(EvenorOdd(n));
     print(PrimeNos(n));
     print(chkRange(n));
@@ -21,6 +26,13 @@ void main() {
 
 bool chkNonNull(int? n) {
   return n != null;
+}
+
+String ans(int n) {
+  if (chkNonNull(n))
+    return "Number is not null";
+  else
+    return "NULL number";
 }
 
 String EvenorOdd(int n) {
@@ -39,24 +51,24 @@ List<int> PrimeNos(int n) {
 }
 
 bool isprime(int n) {
-  for (int i = 2; i  <= sqrt(n); i++) {
+  for (int i = 2; i <= sqrt(n); i++) {
     if (n % i == 0) return false;
   }
   return true;
 }
 
 String chkRange(int n) {
-  switch(n){
-    case>=0 && <=10 :
-    return 'Small Number ';
+  switch (n) {
+    case >= 0 && <= 10:
+      return 'Small Number ';
 
-    case>10 && <=100:
-    return 'Medium Range ';
+    case > 10 && <= 100:
+      return 'Medium Range ';
 
-    case >100 && <=1000:
-    return 'Large Number ';
+    case > 100 && <= 1000:
+      return 'Large Number ';
 
-    default : 
-    return "Whoa! chill , that's too Big";
+    default:
+      return "Whoa! chill , that's too Big";
   }
 }
